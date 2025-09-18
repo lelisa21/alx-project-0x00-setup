6cefd4e46a3c10042ba7532f8bf3e2cf7d069132
Lelisa Hailu <189599041+lelisa21@users.noreply.github.com>
2025-09-18 18:58:21 +0300

    styles maked required index.ts

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -3,7 +3,7 @@ export interface PillProps{
 }
 export interface ButtonProps {
   title: string;
-  styles?: string; 
+  styles: string; 
   size?: "Small" | "Medium" | "Large";
   shapes?: "rounded-sm" | "rounded-md" | "rounded-full";
   className?: string;
